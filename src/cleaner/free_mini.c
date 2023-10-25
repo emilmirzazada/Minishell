b2411575a396f79121484168b67ac1771c95e123
Emil <emirzaza@student.42wolfsburg.de>
2023-10-25 11:26:11 +0200

    add free splits

--- src/cleaner/free_mini.c
+++ src/cleaner/free_mini.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   free_mini.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wrottger <wrottger@student.42.fr>          +#+  +:+       +#+        */
+/*   By: emirzaza <emirzaza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/12 11:18:38 by wrottger          #+#    #+#             */
-/*   Updated: 2023/10/19 18:42:27 by wrottger         ###   ########.fr       */
+/*   Updated: 2023/10/24 21:07:55 by emirzaza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,10 +24,26 @@ static void	free_commands(t_cmd *list)
 	}
 }
 
+static void	free_splits(t_split *list)
+{
+	t_split	*tmp;
+
+	while (list)
+	{
+		tmp = list;
+		list = list->next;
+		free(tmp->arg);
+		free(tmp);
+		tmp = NULL;
+	}
+}
+
 void	free_mini(t_minishell *mini)
 {
 	if (mini->cmd)
 		free_commands(mini->cmd);
+	if (mini->split)
+		free_commands(mini->split);
 }
 
 void	clean_exit(t_minishell *mini, int exit_code)
