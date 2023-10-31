596b5363eef7534fbd32d1cc7ad5b82e53ce3b00
wrottger <wrottger@student.42wolfsburg.de>
2023-10-31 13:17:58 +0100

    fix: echo with multiple -n

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -3,65 +3,46 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: emirzaza <emirzaza@student.42.fr>          +#+  +:+       +#+        */
+/*   By: wrottger <wrottger@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/06 11:23:09 by emirzaza          #+#    #+#             */
-/*   Updated: 2023/10/23 17:18:43 by emirzaza         ###   ########.fr       */
+/*   Updated: 2023/10/31 13:17:10 by wrottger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	echo_no_args(t_cmd *cmd)
+static char	**parse_arguments(char **arg, int *print_newline)
 {
-	if (!cmd->args[1])
+	*print_newline = 1;
+	while (*arg && ft_strncmp(*arg, "-n", 2) == 0)
 	{
-		printf("\n");
-		return (0);
+		*print_newline = 0;
+		arg++;
 	}
-	else if (ft_strcmp(cmd->args[1], "-n") == 0
-		&& !cmd->args[2])
-		return (1);
-	return (0);
+	return (arg);
 }
 
-int	echo_with_new_line(t_cmd *cmd)
+static void	print_arguments(char **arg)
 {
-	int	i;
-
-	i = 1;
-	while (cmd->args[i])
+	while (*arg)
 	{
-		if (i > 1)
+		ft_putstr_fd(*arg, 1);
+		if (*(arg + 1))
 			ft_putstr_fd(" ", 1);
-		ft_putstr_fd(cmd->args[i++], 1);
+		arg++;
 	}
-	ft_putstr_fd("\n", 1);
-	return (0);
-}
-
-int	echo_without_new_line(t_cmd *cmd)
-{
-	int	i;
-
-	i = 2;
-	while (cmd->args[i])
-	{
-		if (i > 2)
-			ft_putstr_fd(" ", 1);
-		ft_putstr_fd(cmd->args[i++], 1);
-	}
-	return (0);
 }
 
 int	ft_echo(t_cmd *cmd)
 {
-	if (echo_no_args(cmd))
-		return (1);
-	else if (cmd->args[1]
-		&& ft_strcmp(cmd->args[1], "-n") == 0)
-		echo_without_new_line(cmd);
-	else
-		echo_with_new_line(cmd);
+	char	**arg;
+	int		print_newline;
+
+	arg = &(cmd->args[1]);
+	arg = parse_arguments(arg, &print_newline);
+	print_arguments(arg);
+	if (print_newline)
+		ft_putstr_fd("\n", 1);
 	return (0);
 }
