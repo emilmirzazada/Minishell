e30f63d8f07179642beca40426d6a24fbe7fc271
Emil <emirzaza@student.42wolfsburg.de>
2023-10-31 19:44:20 +0100

    fixed exit code parsing

--- libft/nbr/ft_atoi.c
+++ libft/nbr/ft_atoi.c
@@ -6,37 +6,50 @@
 /*   By: emirzaza <emirzaza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/19 10:10:09 by emirzaza          #+#    #+#             */
-/*   Updated: 2023/10/19 10:57:26 by emirzaza         ###   ########.fr       */
+/*   Updated: 2023/10/31 19:39:13 by emirzaza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
 
+const char	*atoi_helper(const char *str, long *num, int *negative)
+{
+	while (*num == 0 && (*str == '\n' || *str == '\t' || *str == '\f'
+			|| *str == '\r' || *str == '\v' || *str == ' '))
+		str++;
+	if (*num == 0 && (*str == '-' || *str == '+'))
+		if (*str++ == '-')
+			*negative += 1;
+	while (*num == 0 && *str == '0')
+		str++;
+	return (str);
+}
+
 bool	ft_atoi(const char *str, long *num)
 {
 	int		negative;
+	bool	status;
 
 	*num = 0;
+	status = true;
 	negative = 0;
 	if (!str)
 		return (false);
 	while (*str)
 	{
-		while (*num == 0 && (*str == '\n' || *str == '\t' || *str == '\f'
-				|| *str == '\r' || *str == '\v' || *str == ' '))
-			str++;
-		if (*num == 0 && (*str == '-' || *str == '+'))
-			if (*str++ == '-')
-				negative++;
-		while (*num == 0 && *str == '0')
-			str++;
+		str = atoi_helper(str, num, &negative);
 		if (*str >= '0' && *str <= '9')
 			*num = *num * 10 + *str - '0';
 		else
-			return (false);
+		{
+			if (*str == 0)
+				break ;
+			else
+				status = false;
+		}
 		str++;
 	}
 	if (negative % 2 == 1)
 		*num *= -1;
-	return (true);
+	return (status);
 }
--- src/builtins/exit.c
+++ src/builtins/exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wrottger <wrottger@student.42.fr>          +#+  +:+       +#+        */
+/*   By: emirzaza <emirzaza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 22:09:23 by emirzaza          #+#    #+#             */
-/*   Updated: 2023/10/27 19:20:24 by wrottger         ###   ########.fr       */
+/*   Updated: 2023/10/31 19:41:03 by emirzaza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,22 +19,22 @@ int	ft_exit(t_minishell *mini, t_cmd *cmd)
 
 	value = 0;
 	res = true;
-	if (cmd->args[1])
+	if (cmd->args[1] || ft_strcmp(cmd->args[1], "0") == 0)
 		res = ft_atoi(cmd->args[1], &value);
 	else
 		mini->exit_code = 0;
-	printf("exit\n");
 	if (!res)
 	{
-		ft_putstr_fd("exit: numeric argument required\n", 2);
+		ft_putstr_fd("exit\nexit: numeric argument required\n", 2);
 		mini->exit_code = 255;
 	}
 	else if (cmd->args[1] && cmd->args[2])
 	{
-		ft_putstr_fd("exit: too many arguments\n", 2);
-		mini->exit_code = 1;
-		return (mini->exit_code);
+		ft_putstr_fd("exit\nexit: too many arguments\n", 2);
+		return (mini->exit_code = 1, mini->exit_code);
 	}
+	else
+		ft_putstr_fd("exit\n", 1);
 	if (value != 0)
 		mini->exit_code = value % 256;
 	free_mini(mini);
