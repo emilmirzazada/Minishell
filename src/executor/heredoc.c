015d531233d326560faf81adbbc987040c067f28
wrottger <wrottger@student.42wolfsburg.de>
2023-11-02 18:36:56 +0100

    fix: heredoc norm

--- src/executor/heredoc.c
+++ src/executor/heredoc.c
@@ -6,23 +6,26 @@
 /*   By: wrottger <wrottger@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/13 17:31:35 by wrottger          #+#    #+#             */
-/*   Updated: 2023/11/02 18:29:16 by wrottger         ###   ########.fr       */
+/*   Updated: 2023/11/02 18:36:14 by wrottger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static void	free_null(char *str)
+{
+	if (str)
+		free(str);
+	str = NULL;
+}
 
 static void	get_all_lines(t_minishell *mini, char *delimiter, char *name)
 {
 	char	*temp;
 	int		fd;
 
-	free(mini->input);
-	mini->input = NULL;
-	fd = open(name, O_TRUNC | O_CREAT, 0644);
-	if (fd == -1)
-		perror_exit(name, mini, -1);
-	close(fd);
+	free_null(mini->input);
+	close(open(name, O_TRUNC | O_CREAT, 0644));
 	while (true)
 	{
 		temp = readline("> ");
@@ -30,16 +33,13 @@ static void	get_all_lines(t_minishell *mini, char *delimiter, char *name)
 			break ;
 		mini->input = temp;
 		temp = expand(mini, true);
-		free(mini->input);
-		mini->input = NULL;
+		free_null(mini->input);
 		fd = open(name, O_WRONLY | O_APPEND | O_CREAT, 0644);
 		if (fd == -1)
 			perror_exit(name, mini, -1);
-		write(fd, temp, ft_strlen(temp));
-		write(fd, "\n", 1);
+		write(fd, ft_strjoin(temp, "\n"), ft_strlen(temp) + 1);
 		close(fd);
 		free(temp);
-		temp = NULL;
 	}
 	free(temp);
 }
